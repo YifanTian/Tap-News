f73e9031e1a3f4804c526830c12d773a5d3cdb95
YifanTian <yifant@uci.com>
2017-09-10 14:15:48 -0700

    tap-news ver2

--- common/cloudAMQP_client.py
+++ common/cloudAMQP_client.py
@@ -9,7 +9,7 @@ class CloudAMQPClient:
         self.cloud_amqp_url = cloud_amqp_url
         self.queue_name = queue_name
         self.params = pika.URLParameters(cloud_amqp_url)
-        self.params.socket_time_timeout = 3
+        self.params.socket_timeout = 3
         self.connection = pika.BlockingConnection(self.params)
         self.channel = self.connection.channel()
         self.channel.queue_declare(queue=queue_name)
